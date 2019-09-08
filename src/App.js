cdb09bda4522aefd143c09a0c76489b212f8ae0a
Yuxiang Dai <yuxiang.dai43@gmail.com>
2019-09-08 00:47:54 -0400

    Develop (#2)
    * Add ApiKey and igore it
    
    * Single place filter
    
    * Merge
    
    * Add filter templates
    
    * Add modal
    
    * Refactor Filters and margins
    
    * Make Input and Filter the same length
    
    * Remove config

--- src/App.js
+++ src/App.js
@@ -1,56 +1,147 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import { Map, Marker, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
 
-const mapStyles = {
-  width: '100%',
-  height: '100%',
-};
+import Coupon from './shared/coupon/coupon.jsx';
+import Dropdown from './shared/dropdown/Dropdown';
 
-export class MapContainer extends Component {
-  constructor(props) {
-    super(props);
+import API_KEY from './config.js';
 
-    this.state = {
-      stores: [{lat: 47.49855629475769, lng: -122.14184416996333},
-              {latitude: 47.359423, longitude: -122.021071},
-              {latitude: 47.2052192687988, longitude: -121.988426208496},
-              {latitude: 47.6307081, longitude: -122.1434325},
-              {latitude: 47.3084488, longitude: -122.2140121},
-              {latitude: 47.5524695, longitude: -122.0425407}]
+import 'react-rangeslider/lib/index.css';
+import './App.scss';
+
+class Contents extends Component {
+  state = {
+    position: null,
+    maxPrice: 0,
+    showingInfoWindow: true,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+    console.log("You clicked me!");
+  }
+    
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
     }
+  };
+
+  componentDidMount() {
+    this.renderAutoComplete();
   }
 
-  displayMarkers = () => {
-    return this.state.stores.map((store, index) => {
-      return <Marker key={index} id={index} position={{
-       lat: store.latitude,
-       lng: store.longitude
-     }}
-     onClick={() => console.log("You clicked me!")} />
+  componentDidUpdate(prevProps) {
+    if (this.props !== prevProps.map) this.renderAutoComplete();
+  }
+
+  onSubmit(e) {
+    console.log("You clicked me!")
+    e.preventDefault();
+  }
+
+  renderAutoComplete() {
+    const { google, map } = this.props;
+
+    if (!google || !map) return;
+
+    const autocomplete = new google.maps.places.Autocomplete(this.autocomplete);
+    
+    // Bind the map's bounds (viewport) property to the autocomplete object,
+    // so that the autocomplete requests use the current map bounds for the
+    // bounds option in the request.
+    autocomplete.bindTo('bounds', map);
+
+    // Set the data fields to return when the user selects a place.
+    autocomplete.setFields(
+        ['address_components', 'geometry', 'icon', 'name']);
+    
+    // autocomplete.bindTo('bounds', map);
+    autocomplete.addListener('place_changed', () => {
+      const place = autocomplete.getPlace();
+
+      console.log({place})
+      if (!place.geometry) return;
+
+      if (place.geometry.viewport) map.fitBounds(place.geometry.viewport);
+      else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+
+      this.setState({ position: place.geometry.location });
+    });
+    
+  }
+
+  handleOnChange = (value) => {
+    this.setState({
+      maxPrice: value
     })
   }
 
   render() {
+    const { position, maxPrice } = this.state;
+
     return (
-      <>
-        <div>
-          test
+      <div >
+        <div className="search__form" >
+          <input
+                className="search__places"
+                placeholder="Enter a location"
+                ref={ref => (this.autocomplete = ref)}
+                type="text"
+              />
+              <Dropdown />
+          <div>
+            <span>Lat: {position && position.lat()}</span>
+            <div>Long: {position && position.lng()}</div>
+          </div>
         </div>
-        <Map
-          google={this.props.google}
-          zoom={8}
-          style={mapStyles}
-          initialCenter={{ lat: 47.444, lng: -122.176}}
-        >
-          {this.displayMarkers()}
-        </Map>
-        </>
+        <div >
+          <Map
+            {...this.props}
+            center={position}
+            centerAroundCurrentLocation={false}
+            containerStyle={{
+              height: '400px',
+              position: 'relative',
+              margin: '2.5%',
+              width: '95%'
+            }}>
+            <Marker position={position}
+             onClick={this.onMarkerClick}
+             name={'PennApps'}
+            />
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+                <div>
+                  <Coupon/>
+                </div>
+            </InfoWindow>
+          </Map>
+        </div>
+      </div>
     );
   }
 }
 
+const MapWrapper = props => (
+  <Map className="map" google={props.google} visible={false}>
+    <Contents {...props} />
+  </Map>
+);
+
 export default GoogleApiWrapper({
-  apiKey: 'API_KEY_HERE'
-})(MapContainer);
+  apiKey: API_KEY
+})(MapWrapper);
---
+++ src/shared/Filters.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+  },
+  formControl: {
+    margin: '20px 0px',
+  },
+}));
+
+export default function CheckboxesGroup() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    gilad: true,
+    jason: false,
+    antoine: false,
+  });
+
+  const handleChange = name => event => {
+    setState({ ...state, [name]: event.target.checked });
+  };
+
+  const { gilad, jason, antoine } = state;
+  const error = [gilad, jason, antoine].filter(v => v).length !== 2;
+
+  return (
+    <div className={classes.root}>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Filters</FormLabel>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={gilad} onChange={handleChange('gilad')} value="gilad" />}
+            label="American"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={jason} onChange={handleChange('jason')} value="jason" />}
+            label="Chinese"
+          />
+          <FormControlLabel
+            control={
+              <Checkbox checked={antoine} onChange={handleChange('antoine')} value="antoine" />
+            }
+            label="Mexican"
+          />
+        </FormGroup>
+        {/* <FormHelperText>Be careful</FormHelperText> */}
+      </FormControl>
+    </div>
+  );
+}
